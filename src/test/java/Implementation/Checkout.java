4a0742b147e70cb481a117f305ec45c44b7c5084
Amlan Alok <amlanalok@gmail.com>
2024-06-03 20:12:12 -0500

    all 5 scenarios are passing

--- src/test/java/Implementation/Checkout.java
+++ src/test/java/Implementation/Checkout.java
@@ -5,7 +5,7 @@ public class Checkout {
     int total = 0;
 
     public void add(int count, int price) {
-        this.total = this.total + (count * price);
+        this.total += (count * price);
     }
 
     public int total() {
--- src/test/java/StepDefinitions/CheckoutSteps.java
+++ src/test/java/StepDefinitions/CheckoutSteps.java
@@ -6,23 +6,28 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import java.util.HashMap;
+
 import static org.junit.Assert.assertEquals;
 
 public class CheckoutSteps {
 
-    int bananaPrice = 0;
-    Checkout checkout;
+//    int bananaPrice = 0;
+
+    HashMap<String, Integer> catalog = new HashMap<>();
+    Checkout checkout = new Checkout();
 
     @Given("^the price of a \"(.*?)\" is (\\d+)c$")
     public void thePriceOfAIsC(String name, int price) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        bananaPrice = price;
+//        bananaPrice = price;
+        catalog.put(name, price);
     }
     @When("^I checkout (\\d+) \"(.*?)\"$")
     public void iCheckout(int itemCount, String itemName) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        checkout = new Checkout();
-        checkout.add(itemCount, bananaPrice);
+//        checkout = new Checkout();
+        checkout.add(itemCount, catalog.get(itemName));
     }
     @Then("^the total price should be (\\d+)c$")
     public void theTotalPriceShouldBeC(int total) throws Throwable {
